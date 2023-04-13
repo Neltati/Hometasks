85e5a0c08128008a3cfc4e5015e2b4ae4aed4ea5
Neltati <neltarigame@gmail.com>
2023-04-13 13:39:39 +1200

    Hometasks
    Hometasks

---
+++ ДЗ №1 Переменные/ДЗ №1 Переменные/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ__1_Переменные
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float health;
+            double shieldStrength;
+            byte armore;
+            sbyte hitChanceh;
+            uint criticalHitChance;
+            ushort damdge;
+            int Energy;
+            short targetDistance;
+            long experience;
+            string message;
+            bool targetAlive; 
+        }
+    }
+}
